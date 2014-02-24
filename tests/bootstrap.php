1e51662e0bd92d2e1a81e2af92728bc7c73ce40d
Ruslan Iskhakov <russoz@x-cart.com>
2014-02-24 07:37:10 +0000

    Composer, Config are added

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,8 @@ define('XC5_TEST', true);
 
 define('XC5_TEST_ROOT', __DIR__);
 
+require_once XC5_TEST_ROOT . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+
 // Detect X-Cart 5 root directory
 $__dir = getcwd();
 do {
@@ -29,26 +31,6 @@ if (!defined('XC5_APP_ROOT')) {
 }
 
 require_once XC5_APP_ROOT . DIRECTORY_SEPARATOR . 'top.inc.php';
-
-// Autoloading
-spl_autoload_register(
-    function ($class) {
-        $class = ltrim($class, '\\');
-        list($prefix) = explode('\\', $class, 2);
-        if (in_array($prefix, array('XLiteUnit'))) {
-            require_once (XC5_TEST_ROOT . DIRECTORY_SEPARATOR . 'unit' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
-
-        } elseif (in_array($prefix, array('XLiteIntegration'))) {
-            require_once (XC5_TEST_ROOT . DIRECTORY_SEPARATOR . 'integration' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
-
-        } elseif (in_array($prefix, array('XLiteWeb'))) {
-            require_once (XC5_TEST_ROOT . DIRECTORY_SEPARATOR . 'web' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
-
-        } elseif (in_array($prefix, array('XLiteTest'))) {
-            require_once (XC5_TEST_ROOT . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
-		}
-    }
-);
 
 // Pre-initialzie
 new \XLiteTest\TextUI\ResultPrinter;
---
+++ tests/web/XLiteWeb/AXLiteWeb.php
@@ -0,0 +1,60 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+namespace XLiteWeb;
+use XLiteTest\Framework\Config;
+
+/**
+ * Abstract web test case
+ *
+ * @package XLiteWeb
+ */
+abstract class AXLiteWeb extends \XLiteTest\Framework\TestCase
+{
+    /**
+     * Storefront browser
+     *
+     * @var \WebDriver
+     */
+    protected $storefrontDriver;
+
+    /**
+     * Backend browser
+     *
+     * @var \WebDriver
+     */
+    protected $backendDriver;
+
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     *
+     */
+    public function setUp()
+    {
+
+        $capabilities = array(
+            \WebDriverCapabilityType::BROWSER_NAME => Config::getInstance()->getOptions('web_driver', 'browser_name')
+        );
+
+        // Start storefront browser
+        $this->storefrontDriver = \RemoteWebDriver::create(Config::getInstance()->getOptions('web_driver', 'driver_url'), $capabilities);
+
+        // Start backend browser
+        $this->backendDriver = \RemoteWebDriver::create(Config::getInstance()->getOptions('web_driver', 'driver_url'), $capabilities);
+
+        parent::setUp();
+    }
+
+    /**
+     * Tears down the fixture, for example, close a network connection.
+     * This method is called after a test is executed.
+     */
+    public function tearDown()
+    {
+        $this->storefrontDriver->close();
+        $this->backendDriver->close();
+
+        parent::tearDown();
+    }
+} 
