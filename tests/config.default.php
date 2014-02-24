be1c321d656c59162a274c4d52a3096dd36e4e29
Ruslan Iskhakov <russoz@x-cart.com>
2014-02-24 07:37:10 +0000

    Composer, Config are added

---
+++ tests/config.default.php
@@ -0,0 +1,16 @@
+; <?php /*
+; This file contains a data for X-Cart 5 testing
+
+[admin_user]
+username="rnd_tester@cdev.ru"
+password="master"
+
+[customer_user]
+username="rnd_tester@rrf.ru"
+password="guest"
+
+[web_driver]
+browser_name="chrome"
+driver_url="http://local.dev:4444/wd/hub"
+
+; */ ?>
---
+++ tests/core/XLiteTest/Framework/Config.php
@@ -0,0 +1,76 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+namespace XLiteTest\Framework;
+
+
+/**
+ * Singleton config class
+ */
+class Config
+{
+    /**
+     * Instance
+     *
+     * @var Config
+     */
+    protected static $instance;
+
+    /**
+     * Config settings
+     *
+     * @var array
+     */
+    protected static $config;
+
+    /**
+     * Method to access a singleton
+     *
+     * @return \XLiteTest\Framework\Config
+     */
+    public static function getInstance()
+    {
+        // Create new instance of the object (if it is not already created)
+        if (!isset(static::$instance)) {
+            static::$instance = new Config();
+        }
+        return static::$instance;
+    }
+
+    /**
+     * Protected constructor
+     */
+    protected function __construct()
+    {
+        static::setConfig();
+    }
+
+    /**
+    * Set config options from config file
+    *
+    * @return array
+    */
+    protected function setConfig()
+    {
+        $configFile = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "config.php";
+
+        if (file_exists($configFile) && false !== ($config = parse_ini_file($configFile, true))) {
+            static::$config;
+        } else {
+            die('Config file not found: ' . $configFile);
+        }
+    }
+
+    /**
+     * Get option from $config variable
+     *
+     * @param $section
+     * @param $name
+     * @return string
+     */
+    public function getOptions($section, $name)
+    {
+        return static::$config[$section][$name];
+    }
+
+}
