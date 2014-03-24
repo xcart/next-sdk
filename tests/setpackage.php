6b70db527dce3cdefc7532d596412908a353c921
Ivan Evseev <givi@x-cart.com>
2014-03-24 14:53:51 +0400

    setpackage tool has been added.

---
+++ tests/setpackage.php
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?php
+
+if ($argc != 2) {
+    die('Usage: setpackage <xml file> <package>');
+}
+
+$filename = $argv[1];
+
+$xml = simplexml_load_string(file_get_contents($filename));
+
+$res = $xml->xpath('//testsuite[testcase]');
+foreach ($res as $node) {
+$node->attributes()->name = $argv[2] . '.' . $node->attributes()->name;
+}
+
+$xml->asXML($filename);
